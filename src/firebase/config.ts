0cbc16089cdbdd4a969774c73aac0678f82b4c2b
Link Testing <ailinktst@gmail.com>
2026-02-11 19:26:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8823427019-11572",
+  "appId": "1:549364132400:web:ca7bb05b2df9cb8c50dbf5",
+  "apiKey": "AIzaSyD-ilR_KKj0LsNkRfn8X0hUfL4ia11BEGU",
+  "authDomain": "studio-8823427019-11572.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "549364132400"
+};
